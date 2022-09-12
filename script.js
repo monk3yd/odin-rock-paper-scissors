1068ced571256dd820afc80d564a2aec589f7bc9
monk3yd <jegajardog@gmail.com>
2022-09-12 18:40:46 -0300

    Refactoring, issues with global counter variables

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-// DOM
-const div = document.querySelector("#buttons");
+// DOM ---
+// Choice Results
+const resultsContainer = document.querySelector("#results");
+const paraPlayerResult = document.createElement("p");
+const paraComputerResult = document.createElement("p");
 
-const rockBtn = document.createElement("button");
-const paperBtn = document.createElement("button");
-const scissorBtn = document.createElement("button");
-
-rockBtn.textContent = "Rock";
-paperBtn.textContent = "Paper";
-scissorBtn.textContent = "Scissors";
-
-rockBtn.addEventListener('click', e => {
-    let choice = e.target.innerText;
-    startRound(choice, computerPlay);
-});
-paperBtn.addEventListener('click', e => {
-    let choice = e.target.innerText;
-    startRound(choice, computerPlay);
-});
-scissorBtn.addEventListener('click', e => {
-    let choice = e.target.innerText;
-    startRound(choice, computerPlay)
-});
-
-div.appendChild(rockBtn);
-div.appendChild(paperBtn);
-div.appendChild(scissorBtn);
-
-results = document.querySelector("#results");
-
-playerResult = document.createElement("p");
-computerResult = document.createElement("p");
+// Score
+const scoreContainer = document.querySelector("#score-container");
+const paraScore = document.createElement("p");
 
 // Computers turn
 function computerPlay() {
@@ -46,21 +23,21 @@ function computerPlay() {
 };
 
 // Start a round (1 move each player)
-function startRound(playerSelection, computerPlay) {
-    let playerChoice = playerSelection.toLowerCase();
+function startRound(playerChoice, computerPlay) {
+    playerChoice = playerChoice.toLowerCase();
     let computerChoice = computerPlay().toLowerCase();
 
     // Clean older result
-    playerResult.textContent = '';
-    computerResult.textContent = '';
+    paraPlayerResult.textContent = '';
+    paraComputerResult.textContent = '';
 
     // Fill displayed result with player/computer choices
-    playerResult.textContent = `Player makes: ${playerChoice}`;
-    computerResult.textContent = `Computer makes: ${computerChoice}`;
+    paraPlayerResult.textContent = `Player makes: ${playerChoice}`;
+    paraComputerResult.textContent = `Computer makes: ${computerChoice}`;
 
     // Show results
-    results.appendChild(playerResult);
-    results.appendChild(computerResult);
+    resultsContainer.appendChild(paraPlayerResult);
+    resultsContainer.appendChild(paraComputerResult);
     
     // Check for winner
     if (playerChoice === computerChoice) {
@@ -69,36 +46,59 @@ function startRound(playerSelection, computerPlay) {
     } else if (playerChoice === "rock" && computerChoice === "scissor"  || playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissor" && computerChoice === "paper") {
         // Player win
         return 1;
+        // console.log(`Player wins round ${i + 1}`);
     } else {
         // PC win
         return 2;
+        // console.log(`Computer wins round ${i + 1}`);
     };
 };
 
-// Start game (series of rounds)
-function gameOn() {
-    // Counter score variables
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // Play 5 rounds
-    // for (let i = 0; i < 5; i++) {
-    roundWinner = startRound(playerSelection, computerPlay);
-    // console.log(roundWinner);
-    
-    if (roundWinner === 0) {  // draw
-        console.log(`Round ${i + 1} is a draw`);
-    } else if (roundWinner === 1) {  // player wins
+function calcScore(roundWinner, playerScore, computerScore) {
+    if (roundWinner === 1) {
         playerScore++;
-        console.log(`Player wins round ${i + 1}`);
-    } else {  // computer wins
+    } else if (roundWinner === 2) {
         computerScore++;
-        console.log(`Computer wins round ${i + 1}`);
-    };
-    // Announce winnner
-    if (playerScore > computerScore) {
-        console.log(`Player wins with a score of ${playerScore}/5`);
     } else {
-        console.log(`Computer wins with a score of ${computerScore}/5`);
-    };
+        return "Draw.";
+    }
+    paraScore.textContent = ``;
+    paraScore.textContent = `Player Score: ${playerScore} - Computer Score: ${computerScore}`;
+    scoreContainer.appendChild(paraScore);
+    return [playerScore, computerScore];
 };
+
+function checkEndGame(roundCounter) {
+    if (roundCounter > 5) {
+        // Announce winnner
+        if (playerScore > computerScore) {
+            console.log(`Player wins with a score of ${playerScore}/5`);
+        } else {
+            console.log(`Computer wins with a score of ${computerScore}/5`);
+        };
+    }
+};
+
+// Counter score variables
+let playerScore = 0;
+let computerScore = 0;
+
+// Round counter variable
+let roundCounter = 0;
+
+// Attach events to buttons
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let choice = e.target.innerText;
+        let roundWinner = startRound(choice, computerPlay);
+        let scores = calcScore(roundWinner, playerScore, computerScore);
+        playerScore = scores[0];
+        computerScore = scores[1];
+        roundCounter++;
+        checkEndGame(roundCounter);
+    });
+});
+
+// Play 5 rounds
+console.log(`Round: ${roundCounter}`);
